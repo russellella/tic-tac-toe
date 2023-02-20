537b2cf77a11ab0f10ad202fae6b19af0c381ab5
russellella <116303658+russellella@users.noreply.github.com>
2023-02-20 07:29:28 -0800

    Add player class and check win/tie conditions
    -Added a player class
    -Added method to check win/tie conditions
    -Added method to check if game board has any empty spots
    -Created outline for player turn

--- ttt.rb
+++ ttt.rb
@@ -1,15 +1,76 @@
-# Test Board with arrays
 
-#Creates a blank board
-board = [["_","_","_"],["_","_","_"],["_","_","_"]]
+# class for board
+class Board
 
-def display(arr)
-  puts "  A   B   C"
-  arr.each do |one, two, three|
-    x = 1
-    puts "#{x} #{one} | #{two} | #{three}"
-    x += 1 #Not working - figure out how to increment for each row
+  # Display Board
+  def display(arr)
+    puts "  A   B   C"
+    arr.each do |one, two, three|
+      x = 1
+      puts "#{x} #{one} | #{two} | #{three}"
+      x += 1 #Not working - figure out how to increment for each row
+    end
+  end
+  def blank_board
+    #Creates a blank board
+    bboard = [["_","_","_"],["_","_","_"],["_","_","_"]]
   end
 end
 
+#Need to fix how to display board - using class
 p display(board)
+
+
+# class for players
+class Player(name, mark)
+  def initialize
+    @name = name
+    @mark = mark
+  end
+end
+
+# Check for empty spaces
+def check_empty(arr)
+  arr.any? { |a| a.include?("_") }
+end
+
+
+# method for checking win/tie
+def check_board(arr)
+  # Checks wins:
+  if arr[0][0] == arr[0][1] && arr[0][1] == arr[0][2]
+  
+  elsif arr[1][0] == arr[1][1] && arr[1][1] == arr[1][2]
+    
+  elsif arr[2][0] == arr[2][1] && arr[2][1] == arr[2][2]
+  
+  elsif arr[0][0] == arr[1][0] && arr[1][0] == arr[2][0]
+  
+  elsif arr[0][1] == arr[1][1] && arr[1][1] == arr[2][1]
+  
+  elsif arr[0][2] == arr[1][2] && arr[1][2] == arr[2][2]
+
+  elsif arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]
+  
+  elsif arr[2][0] == arr[1][1] && arr[1][1] == arr[0][2]
+      #Game ends for each of these - pass along mark
+  else
+    puts "This didn't work... :("
+  end
+end
+
+#Turns
+# class for turn?
+def turn()
+if check_empty(board) == false
+  check_board(board)
+else
+  # Prompt player input
+  puts "#{player}'s turn. Where do you want to place your mark?"
+
+end
+
+# Check format (ABC/123)
+  
+# Place player mark
+  board[1][1] = "X"
